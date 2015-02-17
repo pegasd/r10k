4861b0e3e7fd6aed246c2ab307c31b2dc2f53b09
Kirill Kondratenko <cybem@cybem.info>
2015-02-17 14:43:45 +0300

    Implement repository

--- lib/r10k/hg/repository.rb
+++ lib/r10k/hg/repository.rb
@@ -18,13 +18,89 @@ class R10K::HG::Repository
   #   @return [String] The name of the directory
   attr_reader :dirname
 
+  def resolve_tag(tag)
+    return resolve('tags', tag)
+  end
+
+  def resolve_branch(branch)
+    return resolve('branches', branch)
+  end
+
+  def resolve_changeset(changeset)
+    return to_node_id(changeset)
+  end
+
+  def resolve_latest_tag()
+    output = hg ['log', '-r', '"max(tagged())"', '--template',
+                 '"{node}\\n"'], :raise_on_fail => false
+
+    if output.success?
+      output.stdout.lines.first
+    end
+  end
+
+  # @return [Hash<String, String>] A hash of remote names and URIs
+  # @api private
+  def remotes
+    output = hg ['path']
+
+    ret = {}
+    output.stdout.each_line do |line|
+      name, _, url = line.partition(/\s+\=\s+/)
+      ret[name] = url
+    end
+
+    ret
+  end
+
+  def tags
+    return list('tags')
+  end
+
+  def branches
+    return list('branches')
+  end
+
   private
+
+  def resolve(command, name)
+    output = hg [command]
+
+    output.stdout.each_line do |line|
+      hg_name, _, changeset = line.partition(/\s+/)
+      if hg_name == name
+        _, id = parse_changeset(changeset)
+        return to_node_id(id)
+      end
+    end
+  end
+
+  def parse_changeset(changeset)
+    rev, _, id = changeset.partition(/\:/)
+    rev, id
+  end
+
+  def to_node_id(id)
+    output = hg ['log', '-r', id, '--template',
+                 '"{node}\\n"'], :raise_on_fail => false
+
+    if output.success?
+      output.stdout.lines.first
+    end
+  end
+
+  def list(command)
+    entries = []
+    output = hg([command]).stdout
+    output.each_line { |line| entries << line.split[0] }
+    entries
+  end
 
   # Pull changes from the given hg repository
   #
-  # @param source [#to_s] The source repository to pull from
-  def pull(source = 'default')
-    hg ['pull', source]
+  # @param remote [#to_s] The remote name to pull from
+  def pull(remote = 'default')
+    hg ['pull', remote]
   end
 
   # Wrap hg commands
