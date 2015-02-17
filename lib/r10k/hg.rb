ba085bb5b95e22531bfc44b585947d8ad2484330
Kirill Kondratenko <cybem@cybem.info>
2015-02-17 17:34:55 +0300

    Fix module HG

--- lib/r10k/hg.rb
+++ lib/r10k/hg.rb
@@ -1,12 +1,10 @@
 module R10K
-  module Git
+  module HG
     require 'r10k/hg/errors'
 
     require 'r10k/hg/changeset'
-    require 'r10k/hg/tip'
-    #require 'r10k/git/tag'
-    #require 'r10k/git/branch'
-    #require 'r10k/git/remote_head'
+    require 'r10k/git/tag'
+    require 'r10k/git/branch'
 
     require 'r10k/git/repository'
     require 'r10k/git/working_dir'
