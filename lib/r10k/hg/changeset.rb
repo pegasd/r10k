272d43bc41ed898a0d7243ea89c72e84580aca75
Kirill Kondratenko <cybem@cybem.info>
2015-02-17 17:00:54 +0300

    Add changeset

---
+++ lib/r10k/hg/changeset.rb
@@ -0,0 +1,23 @@
+require 'r10k/hg/rev'
+require 'r10k/hg/repository'
+
+# changeset: A changeset is an atomic collection of changes to files in a
+# repository.
+# @see http://mercurial.selenic.com/wiki/ChangeSet
+# @api private
+class R10K::HG::Changeset < R10K::HG::Rev
+
+  # @!attribute [r] changeset
+  #   @return [String] The hg changeset
+  attr_reader :changeset
+  alias :rev :changeset
+
+  def initialize(changeset, repository = nil)
+    @changeset = changeset
+    @repository = repository
+  end
+
+  def fetch?
+    ! resolvable?
+  end
+end
