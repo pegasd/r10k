dd8a5e5309787c3a20dd6404e808e6e71352cb4e
Kirill Kondratenko <cybem@cybem.info>
2015-02-17 15:35:34 +0300

    Add tag

---
+++ lib/r10k/hg/tag.rb
@@ -0,0 +1,23 @@
+require 'r10k/hg/rev'
+require 'r10k/hg/repository'
+
+# tag: A tag is a symbolic identifier for a changeset.
+#
+# @see http://mercurial.selenic.com/wiki/Tag
+# @api private
+class R10K::HG::Tag < R10K::HG::Rev
+
+  # @!attribute [r] tag
+  #   @return [String] The hg tag
+  attr_reader :tag
+  alias :rev :tag
+
+  def initialize(tag, repository = nil)
+    @tag = tag
+    @repository = repository
+  end
+
+  def fetch?
+    ! resolvable?
+  end
+end
